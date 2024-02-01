0496345abe37ec6d109cd99fe76eac0e64ef90d6
nachodiazcode <ignaciosergiodiaz@gmail.com>
2024-02-01 11:49:56 -0300

    dejando una version más estable a la fecha

--- src/app/components/slideshow/slideshow.component.ts
+++ src/app/components/slideshow/slideshow.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SlideshowComponent implements OnInit {
 
-  images = [180, 1070, 1082].map((n) => `https://picsum.photos/id/${n}/1920/330`);
+  images = [180, 1070, 1082].map((n) => `https://picsum.photos/id/${n}`);
 
   constructor() { }
 
